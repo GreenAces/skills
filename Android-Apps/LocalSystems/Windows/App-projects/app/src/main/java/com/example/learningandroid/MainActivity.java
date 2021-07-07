50efb2866a9be3c11e72695bb031f46fc5fe5469
GreenAces <rlabonte98@gmail.com>
2021-07-07 16:57:46 -0400

    Added buttons
    Add, Minus, and Reset Buttons were added

--- Android-Apps/LocalSystems/Windows/App-projects/app/src/main/java/com/example/learningandroid/MainActivity.java
+++ Android-Apps/LocalSystems/Windows/App-projects/app/src/main/java/com/example/learningandroid/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.learningandroid;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+/**
+ * Date 7/7/2021
+ * paused video at 30:38
+ */
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtCounter;
+    private Button btnPlus, btnMinus, btnReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtCounter = findViewById(R.id.txt_counter);
+        btnPlus = findViewById(R.id.btn_plus);
+        btnMinus = findViewById(R.id.btn_minus);
+        btnReset = findViewById(R.id.btn_reset);
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int counter = Integer.parseInt(txtCounter.getText().toString());
+                counter++;
+                txtCounter.setText(counter+"");
+
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int counter = Integer.parseInt(txtCounter.getText().toString());
+                if (counter!=0) {
+                    counter--;
+                    txtCounter.setText(counter+"");
+                }
+
+            }
+
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCounter.setText("0");
+            }
+        });
+
+
     }
 }
