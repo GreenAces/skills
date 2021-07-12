8c327e977c947d4b94dfa731c5641f2064ce82d6
GreenAces <rlabonte98@gmail.com>
2021-07-12 13:53:48 -0400

    Java tutorial 1
    completed -- next lesson operatos

--- Android-Apps/LocalSystems/Windows/IntelliJ-IDEA_projects/src/org/meicode/helloworld/Hello.java
+++ Android-Apps/LocalSystems/Windows/IntelliJ-IDEA_projects/src/org/meicode/helloworld/Hello.java
@@ -2,6 +2,52 @@ package org.meicode.helloworld;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // basic variable declarations
+
+//        int number = 5;
+//        System.out.println(number);
+//        long myLong =5;
+//        System.out.println(myLong);
+//
+//        double myDouble =4.5;
+//        System.out.println(myDouble);
+//
+//        float myFloat = (float) 4.5;
+//        System.out.println(myFloat);
+//
+//        char myChar = 'M';
+//        System.out.println(myChar);
+//
+//        String name = "Kevin";
+//        System.out.println(name);
+//
+//        boolean myBoolean = true;
+//        System.out.println(myBoolean);
+
+        // intro to operators
+
+        int a = 6;
+        int b = 2;
+        int answer = a+b;
+        System.out.println(answer);
+
+        // adding strings
+
+        String firstName = "Kevin";
+        String lastName  = " Derick";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
