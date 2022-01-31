8c9733052aacf8148615c048035dccab43505c24
GreenAces <rlabonte98@gmail.com>
2022-01-31 16:09:04 -0500

    removed 404.php file

--- Websites/5DFbHhuN/php/action_page.php
+++ Websites/5DFbHhuN/php/action_page.php
@@ -4,20 +4,30 @@
 
 <?php
 
-  $servername = "localhost";
+  $servername = "localhost:3306";
   $username = "AdminDemo";
   $pass = "ujDl9b$.Mxy";
   $dbname = "greenace_databaseDemo";
   $conn = mysqli_connect($servername,$username,$pass,$dbname);
+  $valid = "Connection successful!";
 
-  $firstName = $_POST['fname'];
-  $lastName = $_POST['lname'];
 
-  if ($conn) {
+  if(isset($_POST['fname'] && isset($_POST['lname'])) {
 
-    Welcome <?php echo $_POST["fname"]; ?>
+    $firstName = $_POST['fname'];
+    $lastName = $_POST['lname'];
 
-  }else{
+    $sql = "INSERT INTO 'table1' ('fname','lname') VALUES ('$firstName', '$lastName')";
+
+    Welcome,$_POST["fname"];
+
+  ]
+
+  if ($conn == true && $_SERVER["REQUEST_METHOD"]  == "POST") {
+
+    $_POST["valid"];
+
+  }else if{
 
     die("invalid credentials entered" . mysqli_connect_error());
 
