4354cc46ad36589a1e46751952274368462bd922
GreenAces <rlabonte98@gmail.com>
2022-01-31 11:07:53 -0500

    form page updated

--- Websites/5DFbHhuN/php/action_page.php
+++ Websites/5DFbHhuN/php/action_page.php
@@ -1,10 +1,31 @@
 <!DOCTYPE html>
 <html>
 <body>
-  
-  Welcome <?php echo $_POST["fname"]; ?><br>
-  Your age is: <?php echo $_POST["fage"]; ?>
 
+<?php
+
+  $servername = "localhost";
+  $username = "greenace_databaseDemo.table1";
+  $pass = "ujDl9b$.Mxy";
+  $dbname = "AdminDemo";
+  $conn = mysqli_connect($servername,$username,$pass,$dbname);
+
+  $firstName = $_POST['fname'];
+  $lastName = $_POST['lname'];
+
+
+  if ($conn) {
+
+    Welcome <?php echo $_POST["fname"]; ?>
+
+  }else{
+
+    die("invalid credentials entered" . mysqli_connect_error());
+
+  }
+
+
+?>
 
 </body>
 </html>
