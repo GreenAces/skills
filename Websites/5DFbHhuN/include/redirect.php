7c3a5d4ec69538ba3ff6974ca7036f41f756cce6
GreenAces <rlabonte98@gmail.com>
2022-03-01 15:05:39 -0500

    Redirect-php to main site after form completion

---
+++ Websites/5DFbHhuN/include/redirect.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_POST['submit'])){
+// Fetching variables of the form which travels in URL
+$enjoyable = $_POST['enjoyable'];
+$age = $_POST['age'];
+$errors = $_POST['errors'];
+$bugImpactGameplay = $_POST['bugImpactGameplay'];
+$recommenedGame = $_POST['recommenedGame'];
+$AndriodORBrowser =  $_POST['AndriodORBrowser'];
+$rateGame = $_POST['rateGame'];
+if($enjoyable !=''&& $age !=''&& $errors !=''&& $bugImpactGameplay !='' && $recommenedGame !='' && $AndriodORBrowser !='' && $rateGame !='')
+{
+//  To redirect form on a particular page
+header("Location:https://greenaces.site/");
+}
+else{
+?><span><?php echo "Please answer all questions Before submitting the form. You may leave the last question blank.";?></span> <?php
+}
+}
+?>
