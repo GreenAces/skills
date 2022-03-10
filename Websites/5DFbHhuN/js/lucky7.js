d87b5cbea88f60f01c0a015d0f06dbd8c0574c8a
GreenAces <rlabonte98@gmail.com>
2022-03-09 19:00:17 -0500

    improvements to lucky7.html and lucky7.js files

--- Websites/5DFbHhuN/js/lucky7.js
+++ Websites/5DFbHhuN/js/lucky7.js
@@ -5,14 +5,14 @@
 
 
 //function setups here:
-// This generates random number that is range bound from 1 to 10.
+// Each dice is range bound from 1 to 6.
 
 
 
 
 // Event listeners
 document.getElementById("rollButton").addEventListener("click", allFunctions);
-
+document.getElementById("PlayAgainButton").addEventListener("click", reload);
 
 
 
@@ -21,14 +21,230 @@ function rollDice() {
   console.log("rollDice function started.");
 
   //variable declartions
-  let number = Math.ceil((Math.random() * 10));
+  let dice1 = Math.ceil((Math.random() * 6));
+  let dice2 = Math.ceil((Math.random() * 6));
   let mouseClickSound = new Audio('https://greenaces.site/5DFbHhuN/sounds/lucky7/MouseDoubleClick.wav');
   let applauseSound = new Audio('https://greenaces.site/5DFbHhuN/sounds/lucky7/SMALL_CROWD_APPLAUSE.wav');
   mouseClickSound.play();
 
-  if (number === 7) {
+  //logic for diceImages here:
+
+  //logic for dice1
+  if (dice1 === 1) {
+
+    // remove previous dice image
+
+    let elem =  document.createElement("img");
+    elem.src ="";
+    document.getElementById("dice1").appendChild(elem);
+    document.getElementById("dice1").style.width = 95;
+    document.getElementById("dice1").style.height = 71;
+
+    // load new dice image
+    document.getElementById("dice1").innerHTML = '<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/dice1.png" </img>';
+    document.getElementById("dice1").style.width = 95;
+    document.getElementById("dice1").style.height = 71;
+
+  }
+
+  if (dice1 === 2) {
+
+    // remove previous dice image
+
+    let elem =  document.createElement("img");
+    elem.src ="";
+    document.getElementById("dice1").appendChild(elem);
+    document.getElementById("dice1").style.width = 95;
+    document.getElementById("dice1").style.height = 71;
+
+    // load new dice image
+    document.getElementById("dice1").innerHTML = '<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/dice2.png" </img>';
+    document.getElementById("dice1").style.width = 95;
+    document.getElementById("dice1").style.height = 71;
+
+  }
+
+  if (dice1 === 3) {
+
+    // remove previous dice image
+
+    let elem =  document.createElement("img");
+    elem.src ="";
+    document.getElementById("dice1").appendChild(elem);
+    document.getElementById("dice1").style.width = 95;
+    document.getElementById("dice1").style.height = 71;
+
+    // load new dice image
+    document.getElementById("dice1").innerHTML = '<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/dice3.png" </img>';
+    document.getElementById("dice1").style.width = 95;
+    document.getElementById("dice1").style.height = 71;
+
+  }
+
+  if (dice1 === 4) {
+
+    // remove previous dice image
+
+    let elem =  document.createElement("img");
+    elem.src ="";
+    document.getElementById("dice1").appendChild(elem);
+    document.getElementById("dice1").style.width = 95;
+    document.getElementById("dice1").style.height = 71;
+
+    // load new dice image
+    document.getElementById("dice1").innerHTML = '<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/dice4.png" </img>';
+    document.getElementById("dice1").style.width = 95;
+    document.getElementById("dice1").style.height = 71;
+
+  }
+
+  if (dice1 === 5) {
+
+    // remove previous dice image
+
+    let elem =  document.createElement("img");
+    elem.src ="";
+    document.getElementById("dice1").appendChild(elem);
+    document.getElementById("dice1").style.width = 95;
+    document.getElementById("dice1").style.height = 71;
+
+    // load new dice image
+    document.getElementById("dice1").innerHTML = '<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/dice5.png" </img>';
+    document.getElementById("dice1").style.width = 95;
+    document.getElementById("dice1").style.height = 71;
+
+  }
+
+  if (dice1 === 6) {
+
+    // remove previous dice image
+
+    let elem =  document.createElement("img");
+    elem.src ="";
+    document.getElementById("dice1").appendChild(elem);
+    document.getElementById("dice1").style.width = 95;
+    document.getElementById("dice1").style.height = 71;
+
+    // load new dice image
+    document.getElementById("dice1").innerHTML = '<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/dice6.png" </img>';
+    document.getElementById("dice1").style.width = 95;
+    document.getElementById("dice1").style.height = 71;
+
+  }
+
+  //logic for dice2
+  if (dice2 === 1) {
+
+    // remove previous dice image
+
+    let elem =  document.createElement("img");
+    elem.src ="";
+    document.getElementById("dice2").appendChild(elem);
+    document.getElementById("dice2").style.width = 95;
+    document.getElementById("dice2").style.height = 71;
+
+    // load new dice image
+    document.getElementById("dice2").innerHTML = '<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/dice1.png" </img>';
+    document.getElementById("dice2").style.width = 95;
+    document.getElementById("dice2").style.height = 71;
+
+  }
+
+  if (dice2 === 2) {
+
+    // remove previous dice image
+
+    let elem =  document.createElement("img");
+    elem.src ="";
+    document.getElementById("dice2").appendChild(elem);
+    document.getElementById("dice2").style.width = 95;
+    document.getElementById("dice2").style.height = 71;
+
+    // load new dice image
+    document.getElementById("dice2").innerHTML = '<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/dice2.png" </img>';
+    document.getElementById("dice2").style.width = 95;
+    document.getElementById("dice2").style.height = 71;
+
+  }
+
+  if (dice2 === 3) {
+
+    // remove previous dice image
+
+    let elem =  document.createElement("img");
+    elem.src ="";
+    document.getElementById("dice2").appendChild(elem);
+    document.getElementById("dice2").style.width = 95;
+    document.getElementById("dice2").style.height = 71;
+
+    // load new dice image
+    document.getElementById("dice2").innerHTML = '<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/dice3.png" </img>';
+    document.getElementById("dice2").style.width = 95;
+    document.getElementById("dice2").style.height = 71;
+
+  }
+
+  if (dice2 === 4) {
+
+    // remove previous dice image
+
+    let elem =  document.createElement("img");
+    elem.src ="";
+    document.getElementById("dice2").appendChild(elem);
+    document.getElementById("dice2").style.width = 95;
+    document.getElementById("dice2").style.height = 71;
+
+    // load new dice image
+    document.getElementById("dice2").innerHTML = '<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/dice4.png" </img>';
+    document.getElementById("dice2").style.width = 95;
+    document.getElementById("dice2").style.height = 71;
+
+  }
+
+  if (dice2 === 5) {
+
+    // remove previous dice image
+
+    let elem =  document.createElement("img");
+    elem.src ="";
+    document.getElementById("dice2").appendChild(elem);
+    document.getElementById("dice2").style.width = 95;
+    document.getElementById("dice2").style.height = 71;
+
+    // load new dice image
+    document.getElementById("dice2").innerHTML = '<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/dice5.png" </img>';
+    document.getElementById("dice2").style.width = 95;
+    document.getElementById("dice2").style.height = 71;
+
+  }
+
+  if (dice2 === 6) {
+
+    // remove previous dice image
+
+    let elem =  document.createElement("img");
+    elem.src ="";
+    document.getElementById("dice2").appendChild(elem);
+    document.getElementById("dice2").style.width = 95;
+    document.getElementById("dice2").style.height = 71;
+
+    // load new dice image
+    document.getElementById("dice2").innerHTML = '<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/dice6.png" </img>';
+    document.getElementById("dice2").style.width = 95;
+    document.getElementById("dice2").style.height = 71;
+
+  }
+
+
+
+//logic for generating numbers for each dice
+
+
+
+  if (dice1 + dice2 === 7) {
 
     //inform player1 of victory and get feedback
+    document.getElementById("displayRandomNumber").innerHTML = dice1+dice2;
     document.getElementById("InformPlayerWon").innerHTML=("Congratulations you won!!!");
     document.getElementById("InformPlayerWonDetails").innerHTML=("Click the link below to leave feedback: ");
     document.getElementById("sendLink").innerHTML = "Give feedback";
@@ -42,14 +258,22 @@ function rollDice() {
     //disable roll button if player won
     document.getElementById("rollButton").disabled = true;
 
-    //Get status of rounds played
-    getRounds();
+    //debugging here:
+    console.log("dice1: " +dice1);
+    console.log("dice2: " +dice2);
 
-    //reload game can be loaded here
+    //allow player to play again without refreshing the page
+    document.getElementById("PlayAgainButton").innerHTML='<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/play-again-button.png" </img>';
+    document.getElementById("PlayAgainButton").style.width = 160;
+    document.getElementById("PlayAgainButton").style.height = 77;
 
 
 
-  } else if (!number === 7){
+    return dice1+dice2;
+
+  }
+
+  if (!dice1 + dice2 === 7) {
 
   }
 
@@ -58,47 +282,25 @@ function rollDice() {
 
   document.getElementById("InformPlayerLoss").innerHTML=("Better luck next time...");
 
-  document.getElementById("displayRandomNumber").innerHTML = number;
+  document.getElementById("displayRandomNumber").innerHTML = dice1+dice2;
 
   //give the user the option to start the game over without refreshing the page.
 
 
+  //debugging here:
+  console.log("dice1*: " +dice1);
+  console.log("dice2*: " +dice2);
 
 
-  return number;
+  return dice1+dice2;
 
 
 }// end of rollButton function
 
-// This function will determine if number is odd or even.
-
-function oddOrEven() {
-
-  console.log("oddOrEven function started.");
-
-
-  //varible declartions
-  let number = play();
-  let display;
-
-
-  if (number === 2 || number === 4 || number === 6 || number === 8 || number === 10) {
-   //  document.getElementById("data1").innerHTML = number;
-    display = "Even number"
-
-  }else if (number === 1 || number === 3 || number === 5 || number === 7  || number === 9){
-  //  document.getElementById("data1").innerHTML = number;
-    display = "Odd number"
-
-
-  }
-
-  document.getElementById("data3").innerHTML = display;
 
 
 
-}//end of oddOrEven function
-
+//count the number of rounds -- game ends when round is 3
 
 
 let count = 0; // This variable needs to be kept outside or it will not work properly...
@@ -110,25 +312,44 @@ function getRounds() {
         //varible declartions
        count = parseInt(count) + parseInt(1);
        let x = document.getElementById("data2");
-       x.innerHTML= "Round ("+ count +")";
+       x.innerHTML= "Round#" + count;
+
 
 
        if (count == 3) {
 
           // inform the user first that the game is over.
-          document.getElementById("displayGameAlert").innerHTML=("The game is over because you reached the maximum number of rounds " + count);
+          document.getElementById("displayGameAlert").innerHTML=("The game is over because you reached the maximum number of rounds. " + "(" + count + ")");
 
           // disable button to prevent more mouse clicks.
           document.getElementById("rollButton").disabled = true;
 
           // give the user the option to start the game over without refreshing the page.
           document.getElementById("PlayAgainButton").innerHTML='<img src ="https://greenaces.site/5DFbHhuN/images/lucky7_game_images/play-again-button.png" </img>';
-        //  document.getElementById("PlayAgainButton").innerHTML = '<button onclick="refreshPage()">Play Again?</button>';
+          document.getElementById("PlayAgainButton").style.width = 160;
+          document.getElementById("PlayAgainButton").style.height = 77;
+
 
        }
 
-
 }//end of getRounds function
+
+
+
+
+
+//This is the function for the PlayAgainButton
+//It simply reloads the page
+
+function reload() {
+
+
+  console.log("reload function started.");
+
+  window.location.reload();
+
+
+}//end of reload function
 
 
 
@@ -136,7 +357,6 @@ function getRounds() {
 function allFunctions() {
 
   rollDice();
-  oddOrEven();
   getRounds();
 
 
