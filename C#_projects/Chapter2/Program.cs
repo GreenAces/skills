af222bc3dd4066033ada1f883cbd112ef31b0fc4
Robert <RLabonte98@gmail.com>
2022-07-04 21:42:13 -0400

    Chapter 2
    Arrays on page 83

---
+++ C#_projects/Chapter2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter2
+{
+     class MainClass
+    
+    {
+        static void Main(string[] args)
+        {
+
+            //chapter 2 -- working with string variables 
+
+            //chapter 2 -- working with string variables 
+            //string myName = "Corey";
+            //int numberOfLetters = myName.Length;
+
+            //Console.WriteLine("There are " + myName.Length + " number of letters in " + myName);
+
+            //string mySentence = "A sentence with the word hello";
+
+            //if (mySentence.IndexOf("Hello") >= 0) Console.WriteLine("Hello is part of the sentence.");
+
+            //else Console.WriteLine("Hello is NOT included in the sentence");
+
+            //if (mySentence.IndexOf("Hi") >= 0) Console.WriteLine("Hi is part of the sentence");
+
+            //else Console.WriteLine("Hi is NOT included in the sentence");
+
+            //string mySentence2 = "This sentence is long and needs to be shorter";
+            //string shorterSentence = mySentence2.Substring(0, 14);//Substring method shortens text and starts @ index of 0 before displaying context of new string
+            //Console.WriteLine("Output of shorterSentence: " + shorterSentence);
+
+            //string mySentence3 = "This sentence is long and needs to be shorter";
+            //string modifiedSentence = mySentence3.Replace("is long", "is way too long");//Replace method replaces string1 with string2
+            //string upperCaseSentence = mySentence3.ToUpper();
+            //string lowerCaseSentence = mySentence3.ToLower();
+
+            //Console.WriteLine("Output of modifiedSentence: " + modifiedSentence);
+            //Console.WriteLine("Output of upperCaseSentence: " + upperCaseSentence);
+            //Console.WriteLine("Output of lowerCaseSentence: " + lowerCaseSentence);
+
+            //Working with number variables
+
+            //Random r = new Random();
+
+            //int randomNumber = r.Next(0, 100); //Random methods picks a number between 0 and 100 for the declared variable 
+
+            //if (randomNumber < 70) Console.WriteLine("There was a 70% chance for this number to be displayed");
+
+            //else Console.WriteLine("There was a 30% chance for this number to be displayed");
+
+            //int minimum = Math.Min(2, 3);
+            //int maximum = Math.Max(100, 900);
+
+            //Console.WriteLine("The smallest number between 2 and 3 is " + minimum);
+            //Console.WriteLine("The greatest number between 100 and 900 is " + maximum);
+
+            //Switch Statements
+
+           
+
+            Console.WriteLine("Please pick your choice 1-3");
+
+            string answer = Console.ReadLine(); //pauses terminal and waits for user input
+
+            int choice = Int32.Parse(answer); //converts string into an integer so that the switch can read it properly
+
+
+            switch (choice) 
+            {
+
+                case 1:
+                    Console.WriteLine("You chose 1");
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("You chose 2");
+
+                    break;
+
+                case 3:
+                    Console.WriteLine("You chose 3");
+
+                    break;
+
+                default:
+                    Console.WriteLine("Default option");
+
+                    break;
+
+
+            }//end of switch statement 
+
+            Console.WriteLine("We have exited the switch statement");
+
+
+
+
+
+
+
+
+
+
+
+
+        }//end of void main 
+
+
+    }//end of main class
+
+
+
+}//end of namespace Chapter2
--- C#_projects/myFirstProject/Program.cs
+++ C#_projects/myFirstProject/Program.cs
@@ -9,38 +9,43 @@ namespace myFirstProject
     class MainClass
 
     {
-        
+
         static void Main(string[] args)
-            
+
         {
-            
-            
-  
-           string myName = "Robert"; // the text is declared using double quotes
-           int dateOfBirth = 1987;
-           int currentYear = 2022; // the year needs no decimals and is declared as an integer 
-           int age = currentYear - dateOfBirth;
-           float temp = 86.5f; //the temp is declared as a float (i.e.. with decimals NOTE-- it needs the "f" at the end)
 
-           int myAge = calculateAge(1990);
 
-          Console.WriteLine("My age is " + myAge);
+
+            string myName = "Robert"; // the text is declared using double quotes
+            int dateOfBirth = 1992;
+            int currentYear = 2022; // the year needs no decimals and is declared as an integer 
+            int age = currentYear - dateOfBirth;
+            float temp = 86.5f; //the temp is declared as a float (i.e.. with decimals NOTE-- it needs the "f" at the end)
+            int units = 500;    //variable is initialized here instead 
+            int pricePerUnit = 4; //variable is initialized here instead 
+            
+
+
+            //creating a method # 1
+            int myAge = calculateAge(1992);
+
+            Console.WriteLine("My age is (2) " + myAge);
 
             Console.WriteLine("My age is " + age); //this is out you output to console using C#
 
-           Console.WriteLine("Please enter your name: ");
+            Console.WriteLine("Please enter your name: ");
 
-           string nameOfUser = Console.ReadLine();
+            string nameOfUser = Console.ReadLine();
 
-          // if (nameOfUser == myName || nameOfUser == "robert") Console.WriteLine("Hello, Robert");
-
-
-         //  else (nameOfUser != myName || nameOfUser != "robert") Console.WriteLine("Hello Stranger");
+            // if (nameOfUser == myName || nameOfUser == "robert") Console.WriteLine("Hello, Robert");
 
 
+            //  else (nameOfUser != myName || nameOfUser != "robert") Console.WriteLine("Hello Stranger");
 
 
-             Console.WriteLine("What is 1 + 1");
+            
+
+            Console.WriteLine("What is 1 + 1");
 
 
             //convert string to number 
@@ -48,55 +53,121 @@ namespace myFirstProject
             int result = Int32.Parse(resultAsString);
 
 
-             if (result == 2) Console.WriteLine("Well done"); // notice no brackets are needed here 
+            if (result == 2) Console.WriteLine("Well done"); // notice no brackets are needed here 
 
-             else Console.WriteLine("Sorry, wrong answer"); // no brackets are needed here either 
-
-
-   
-      
+            else Console.WriteLine("Sorry, wrong answer"); // no brackets are needed here either 
 
 
+            //myMemberVariable was created with myMethod but it is given a value from the MainClass over here instead:
 
-          
+            myMemberVariable = 1;
+
+            Console.WriteLine("The value of myMemberVariable before calling the method is " + myMemberVariable);
+
+            //calling myMethod from the MainClass over here: Notice how the value is changed. 
+            //Conclusion: When calling a function from static void Main(string[] args) it will have higher priority
+            //and will be executed first before calling it from a different class
+            //(tip) a static method should only call a method that is also static
+             myMethod();
 
 
-       
+
+            //calling method # 2 here 
+            calculatePrice(units,pricePerUnit);
 
 
-        }
+            //calling a method with no parameter via the MainClass #3
+            getFullName();
 
 
 
- 
-        //calling a method with a parameter via the main function 
 
-        public static int calculateAge (int yearOfBirth)
+            
+
+
+
+        }//void main ends here 
+
+
+
+
+        //creating a method with a parameter via the MainClass #1
+        public static int calculateAge(int yearOfBirth)
         {
-            return (2018 - yearOfBirth);
+            return (2022 - yearOfBirth);
         }
 
+        //creating a method with a parameter via the MainClass #2
+        public static int calculatePrice (int nbItems, int unitPrice)
+        {
+            int finalPrice = nbItems * unitPrice;
+
+            Console.WriteLine("The final price is " + finalPrice);
+            return (finalPrice);
+        }
+
+     
 
         //creating methods outside of the main function 
 
-        public int myVar;
+        public int myVar;// public local variable can be accessed for methods 1 and 2
+        public static int myMemberVariable;
 
-        public void method1() 
+
+        //new variables created for method #3
+        public static string primaryName;
+        public static string secondaryName;
+
+        public void method1()
 
         {
             myVar = 0;
         }
 
 
-        public void method2() 
+        public void method2()
         {
 
-            myVar = + 1;
+            myVar = +1;
 
-            
+
         }
+
+        public static void myMethod() 
+        
+        {
+            myMemberVariable = 3;
+            Console.WriteLine("The value of myMemberVariable is " + myMemberVariable);
+
+            //NOTE: no return key is needed regarding void methods
+        }
+
+ 
+
+        public static void getFullName()
+
+        {
+
+            Console.WriteLine("Enter your friends first name:");
+            primaryName = Console.ReadLine();
+            Console.WriteLine("Awesome! Now enter your friends last name:");
+            secondaryName = Console.ReadLine();
+            Console.WriteLine("Got it! Your friends full name is " + primaryName + " " + secondaryName);
+            
+            //NOTE: no return key is needed regarding void methods
+
+        }
+
+    
 
        
 
-    }
-}
+
+       
+
+    }//main class ends here 
+
+
+
+
+}//namespace project ends here 
