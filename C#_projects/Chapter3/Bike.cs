1fb09e724dbcc0e2a9669391ebc30ac9a5b514d7
Robert <RLabonte98@gmail.com>
2022-07-05 22:03:07 -0400

    Chapter 3 started
    added new class on page 116

---
+++ C#_projects/Chapter3/Bike.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter3
+{   
+
+    //A class is a bundle of related code
+    //It can be used as a blueprint to create objects (OOP)
+    public class Bike
+    {
+        private float speed;
+        private int color;
+
+        public Bike()
+        {
+            //a constructor is created within a class, it can be left empty or deleted.
+            //A constructor is a special method that is used to initialize objects.
+            //You can think of a contructor as a unique trait that only that particular object can have. (example: class --> person, constructor --> fingerprint ID)
+
+        }//public Bike constructor
+
+        public void accelerate() 
+        {
+            speed++;
+        }
+
+        public void turnRight() 
+        {
+            
+        
+        }
+
+        private void calculateDistance() 
+        {
+        
+        
+        }
+
+        //a get and set method allows you to access and modify data
+        public void setSpeed (float newSpeed) 
+        {
+
+            speed = newSpeed;
+        }
+
+        public float getSpeed() 
+        {
+            return speed; //(tip) only a get method will require the return keyword
+
+        }
+
+        /* The new way to create a get and set method is as follows:
+           
+            private floot speed
+            public float speed
+
+            {
+                get {return speed;}
+                set {speed = value;}
+            
+            }
+         
+         
+         
+         */
+       
+
+    }//public class Bike
+
+}//end of namespace Chapter3
---
+++ C#_projects/Chapter3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bike myBike = new Bike(); //initializing Bike class here 
+            Console.WriteLine("Initial Speed= " + myBike.getSpeed()); //using get method to access speed of bike
+            myBike.setSpeed(23.0f); //using set method to change the speed of the bike (note: decimal requires f handle)
+            Console.WriteLine("New Speed= " + myBike.getSpeed());
+        }
+    }
+}
