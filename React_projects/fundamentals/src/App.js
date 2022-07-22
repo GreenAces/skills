cefe931f3dee9100a5d5b4f2cedc6ebea2c04d68
Robert <RLabonte98@gmail.com>
2022-07-21 22:23:39 -0400

    fundamentals of react
    page 23

--- React_projects/fundamentals/src/App.js
+++ React_projects/fundamentals/src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const title = 'React';
+
 function App() {
+
+// you can do something in between
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello {title}</h1>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text"/>
         <a
           className="App-link"
           href="https://reactjs.org"
