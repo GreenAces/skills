32c1a54a42c630934e3a26cccb6a3ba4630ccf7a
Robert <RLabonte98@gmail.com>
2022-07-22 22:51:32 -0400

    list
    page 28

--- React_projects/fundamentals/src/App.js
+++ React_projects/fundamentals/src/App.js
@@ -1,8 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+const list =
+[
+  {
+  title: 'React',
+  url: 'https://reactjs.org',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
 
-const title = 'React';
+ {
+   title: 'Redux',
+   url: 'https://redux.js.org',
+   author: 'Dan Abramov, Andrew Clark',
+   num_comments: 2,
+   points: 5,
+   objectID: 1,
+
+ },
+
+];
+
 
 function App() {
 
@@ -13,10 +34,22 @@ function App() {
     <div className="App">
 
 
-        <h1>Hello {title}</h1>
-        <p>Hello, Samantha! ;)</p>
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text"/>
+      <h1> My Hacker Stories</h1>
+
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
+
+      <hr />
+
+      <ul>
+      {list.map(function(item){
+
+        return <li> {item.objectID}</li>;
+      })}
+      </ul>
+
+
 
 
     </div>
