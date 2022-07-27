0abeb2aa61fc2b0a12b7d00c8131344c72e2aa1a
Robert <RLabonte98@gmail.com>
2022-07-26 22:06:21 -0400

    page 33

--- React_projects/fundamentals/src/App.js
+++ React_projects/fundamentals/src/App.js
@@ -37,23 +37,55 @@ function App() {
       <h1> My Hacker Stories</h1>
 
 
+
       <label htmlFor="search">Search: </label>
       <input id="search" type="text"/>
 
+      <Search />
+
       <hr />
 
+
+
       <ul>
+
       {list.map(function(item){
 
-        return <li> {item.objectID}</li>;
+        return (
+          <li key={item.objectID}>
+          <span>
+          <a href ={item.url} > {item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+
+
+          </li>
+          );
+
       })}
       </ul>
 
 
 
-
+      <List />
     </div>
   );
+}
+
+function Search ()
+{
+  return
+  (
+
+    <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text"/>
+    </div>
+
+  );
+
 }
 
 export default App;
