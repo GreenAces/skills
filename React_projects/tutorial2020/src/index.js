71e609719177d3be5e16e36c7fd54705ac1edc64
Robert <RLabonte98@gmail.com>
2022-07-29 21:57:39 -0400

    React Components
    See comments above for more details

--- React_projects/tutorial2020/src/index.js
+++ React_projects/tutorial2020/src/index.js
@@ -1,37 +1,37 @@
-import React from "react"; //allows you to write JSX
-import ReactDOM from "react-dom";
+import React from 'react'; //allows you to write JSX
+import ReactDOM from 'react-dom';
 
 /*
 video 2020 @ 1:34:27
-Video 2022 @ 1:00:00
+Video 2022 @ 1:03:00
 
 JSX Rules:
 
 return a single element
 use div / section / article or Fragment but dont create too many div tags
-use camelCase for html attribute
-class name instead of class
+use camelCase (not pascal) for html attribute
+className instead of class
 close every element
-formatting (use single or double quotes but be consistent with them)
+formatting (use single or double quotes but be consistent with just one type)
 
-Nested Components or React tools look like this => <Tag />
+Nested Components or React tools look like this => <ComponentName />
 
 */
 
-const page = (
 
-  <div>
-  <img src="C:\Users\Robert\Downloads\Chrome-downloads\sample.png" width="100px" height="100px"/>
-  <h1>My awesome website in React</h1>
-  <h3>Reasons I love React</h3>
-  <ul>
-  <li>It's composable</li>
-  <li>It's declarative</li>
-  <li>It's a hirable skill</li>
-  <li>It's actively maintained by skilled people</li>
-  </ul>
-  </div>
+function BodyPage () {
 
-)
+  return (
+    <div>
+    <h2>Why I love React: </h2>
+    <ul>
+    <li>It has an awesome library and you can hang out with the kool kids ;)</li>
+    <li>You can get a job as a front end developer</li>
+    <li>It's mobile friendly</li>
+    </ul>
+    </div>
+  )
+}
 
-ReactDOM.render(page, document.getElementById("root"));
+
+ReactDOM.render(<BodyPage />,document.getElementById("root"));
